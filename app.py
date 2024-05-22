273656558dea82ede49276e6ee284d3bf7f28cb1
Daniel Atik <danielatik@users.noreply.github.com>
2024-05-21 20:04:55 -0400

    Test

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from dotenv import load_dotenv
 load_dotenv()
 logging.basicConfig(filename='chat.log', level=logging.INFO)
 
+# test
+
+
 # st.markdown("<!-- Hotjar Tracking Code for OM Iris --><script>(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3602316,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>")
 # Hello
 st.set_page_config(layout="wide")  
